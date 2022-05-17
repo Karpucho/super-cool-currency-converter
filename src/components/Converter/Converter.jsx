cb3e093871cc7baef67f6b7f884f78cdcdf477e6
Igor Karpov <karpov.zgr@mail.ru>
2022-05-17 19:24:32 +0300

    converter begin

--- src/components/Converter/Converter.jsx
+++ src/components/Converter/Converter.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
+import { useState } from 'react';
+import axios from 'axios';
 import './converter.css'
 
 function Converter() {
+
+  const [convertCurrency, setConvertCurrency] = useState('');
+  const [textInput, setTextInput] = useState('');
+
+  const findValueForConvert = (obj, val) => {
+    const newValue = Object.entries(obj).find(([key]) => key === val);
+    return newValue[1];
+  };
+
+  const getCourse = (number, from, to) => {
+    axios.get(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${from}`)
+      .then(data => {
+        const valueToConvert = findValueForConvert(data.data.conversion_rates, to);
+        const result = (valueToConvert * number).toFixed(2);
+        setConvertCurrency(result);
+      })
+      .catch(() => setConvertCurrency('Error. Please, try again'));
+  };
+
+  const getTextInput = () => {
+    const newStr = textInput.replace(/[^a-zA-Z0-9]/gi, '');
+    const numCourse = Number(newStr.replace(/\D+/, ''));
+    const fromCurrency = newStr.slice(-8, -5);
+    const toCurrency = newStr.slice(-3).toUpperCase();
+    getCourse(numCourse, fromCurrency, toCurrency);
+  };
+
+  const onKeyDown = (event) => {
+    if (event.code === 'Enter' || event.code === 'NumpadEnter') {
+      getTextInput();
+    }
+  };
+
+
   return (
     <div>
-      converter
+    <input type='text' value = {textInput} onChange={event => setTextInput(event.target.value)} />
+      {convertCurrency}
     </div>
   );
 }
--- src/components/Courses/Courses.jsx
+++ src/components/Courses/Courses.jsx
@@ -1,10 +1,50 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './courses.css'
+const apiKey = '18585d0d3a6b8701388a2844';
 
 function Courses() {
+  const [currency, setCurrency] = useState({ rub: 'RUB', usd: 'USD', eur: 'EUR'});
+  const [listCurrency, setListCurrency] = useState({});
+
+  const getCurrency = currency => {
+    axios.get(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/${currency}`)
+      .then(res => {
+        setListCurrency(res.data.conversion_rates);
+      })
+      .catch(() => setListCurrency({ 'currency not found': '' }));
+  };
+
+  // useEffect(() => {
+  //   const langBrowser = (navigator.language || navigator.userLanguage).substr(0, 2);
+  //   console.log(currency, 'ЛАНГ');
+  //   if (langBrowser === 'ru') {
+  //     getCurrency(currency.rub);
+  //   } else if (langBrowser === 'en') {
+  //     getCurrency(currency.usd);
+  //   }
+  // }, []);
+
+  useEffect(() => {
+  
+      getCurrency(currency.rub);
+    
+  }, [currency]);
+
   return (
     <div>
-      corses
+          <button onClick={() => getCurrency(currency.rub)}>рубли</button>
+          <button onClick={() => getCurrency(currency.usd)}>доллары</button>
+          <button onClick={() => getCurrency(currency.eur)}>евро</button>
+
+           {Object.entries(listCurrency).map(([currency, val]) => (
+            <div key={currency}>
+              <span>{val}</span>
+              <p>{currency}</p>
+            </div>
+          ))}
     </div>
   );
 }
